0a0569d0721b045b468f5a9b175f0be80f54170f
MZagorski <mateuszzagorski1@gmail.com>
2023-09-22 20:43:06 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import './sass/main.scss';
+import './scss/main.scss';
+import { searchMovies, trendingMovies } from './js/handler';
+
+window.onload = trendingMovies();
+
+const searchForm = document.querySelector('.header__nav-form');
+
+searchForm.addEventListener('submit', searchMovies);
